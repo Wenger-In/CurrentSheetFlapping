cdab17bf5c3d9a4726d8da8e508f7c7ac37e456b
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-11-15 16:19:17 +0800

    Update plot_relative_position.m

--- plot_relative_position.m
+++ plot_relative_position.m
@@ -5,6 +5,9 @@ Rs = 6.955e5; % [km]
 %% Earth position in HCI coordinate, obtained from get_Earth_position.py
 pos_earth_HCI = [41.31776271, -4.80444563, 0.98379865]; % (lon, lat, distance) in (deg, deg, AU)
 
+%% STEREO-A position in HCI coordinate, obtained from cdaweb
+pos_stereo_HCI = [345, 1.8]; % (lon, lat) in (deg, deg)
+
 %% PSP position in HCI coordinate
 % import data
 spc_dir = 'E:\Research\Data\PSP\Encounter 7\';
@@ -22,9 +25,9 @@ pos_psp_HCI = sc_pos_HCI(num_epoch/2,:);
 
 %% plot relative position
 figure()
-scatter(cosd(pos_earth_HCI(1)),sind(pos_earth_HCI(1))); hold on
+scatter(10*cosd(pos_earth_HCI(1)),10*sind(pos_earth_HCI(1))); hold on
+scatter(10*cosd(pos_stereo_HCI(1)),10*sind(pos_stereo_HCI(1))); hold on
 plot(sc_pos_HCI(:,1),sc_pos_HCI(:,2)); hold on
 scatter(pos_psp_HCI(1),pos_psp_HCI(2));
+legend();
 axis equal
-xlim([0,20])
-ylim([0,20])
